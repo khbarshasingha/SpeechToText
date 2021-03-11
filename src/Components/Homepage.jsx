6272852b2bdb7ddf65de40e18c97c7d267560955
Kh Barsha Singha <khbarshasingha@gmail.com>
2021-03-11 08:13:39 +0530

    skeletal design with speech to text conv implementation

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Container, Button } from "react-bootstrap";
 import SpeechRecognition, {
   useSpeechRecognition
 } from "react-speech-recognition";
 
 export const Homepage = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition();
+  const commands = [
+    {
+      command: "open *",
+      callback: website => {
+        window.open("http://" + website.split(" ").join(""));
+      }
+    }
+  ];
+
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
   const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef(null);
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return <div>Browser is not Support Speech Recognition.</div>;
+  }
+  const handleListening = () => {
+    setIsListening(true);
+    SpeechRecognition.startListening({
+      continuous: true
+    });
+  };
+  const handleStop = () => {
+    setIsListening(false);
+    SpeechRecognition.stopListening();
+  };
   return (
     <Container>
-      <Button>Start Listening</Button>
-      <p>Speech</p>
-      <Button>Stop</Button>
+      <Button onClick={handleListening}>Start Listening</Button>
+      <p>{transcript}</p>
+      <Button onClick={handleStop}>Stop</Button>
     </Container>
   );
 };
